eed4b645d06e7d0fab38e28845980556954e85ae
Avi Oza <avioza@gmail.com>
2019-11-29 20:33:27 -0500

    add identifier for message

--- airvpn_checker/check_airvpn.py
+++ airvpn_checker/check_airvpn.py
@@ -12,6 +12,7 @@ q = sqs.get_queue_by_name(QueueName='low-pri-nagios-alerts.fifo')
 def get_airvpn_status(vpn_api_token, expected_session_count):
     url = 'https://airvpn.org/api/'
     exit_code = 0
+    message_text = "AirVPN\n"
 
     resp = requests.get(url, params={'service': 'userinfo',
                                      'format': 'json',
@@ -19,16 +20,16 @@ def get_airvpn_status(vpn_api_token, expected_session_count):
     try:
         resp.raise_for_status()
     except requests.HTTPError as e:
-        message_text = 'Exception when getting status: {}'.format(e)
+        message_text += 'Exception when getting status: {}'.format(e)
         exit_code = 2
         return message_text, exit_code
 
     data = resp.json()
     if 'sessions' not in data:
-        message_text = "Could not find any sessions"
+        message_text += "Could not find any sessions"
         exit_code = 2
     else:
-        message_text = 'AirVPN\nSessions currently connected: {}. Expiration date: {}'.format(",".join([d['device_name'] for d in data['sessions']]), data['user']['expiration_date'])
+        message_text = 'Sessions currently connected: {}. Expiration date: {}'.format(",".join([d['device_name'] for d in data['sessions']]), data['user']['expiration_date'])
         
         if len(data['sessions']) < expected_session_count:
             exit_code = 2
@@ -46,4 +47,4 @@ def lambda_handler(event, context):
     print(status_text, exit_code)
 
 if __name__ == '__main__':
-    lambda_handler(None)
+    lambda_handler(None, None)
