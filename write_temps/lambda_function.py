1920d4f886e671c9ee9f9274af64a58836031b6e
Avinash <avioza@gmail.com>
2020-11-15 19:17:51 -0500

    remove old try/except logic

--- write_temps/lambda_function.py
+++ write_temps/lambda_function.py
@@ -1,5 +1,5 @@
 import json
-import datetime
+
 import arrow
 import boto3
 
@@ -25,25 +25,14 @@ def lambda_handler(event, context):
         else:
             print("Got messages")
             for one_entry in entries:
-                try:
-                    dt_obj = arrow.get(one_entry['status_time_utc'])
-                    print("Got utc timestamp")
-                except KeyError:
-                    dt_obj = arrow.get(datetime.datetime.strptime(one_entry['status_time'], '%Y-%m-%d %I:%M:%S %p')).replace(tzinfo='America/New_York').to('utc')
-                except Exception as e:
-                    print("Skipping message due to {}. message was: {}".format(str(e), one_entry))
-                else:
+                dt_obj = arrow.get(one_entry['status_time_utc'])
+                dt_str = dt_obj.isoformat()
 
-                    dt_str = dt_obj.isoformat()
-
-                    key_name = 'temperature+{}+{}'.format(one_entry['sensor_name'].upper(), dt_obj.date().strftime('%Y%m'))
-                    ddb.put_item(TableName='dataTable', Item={
-                        'key_name': {"S": key_name},
-                        'timestamp': {"S": dt_str },
-                        'reading_value': {"N" : str(one_entry['raw_value']) }
-                        }, ReturnConsumedCapacity='TOTAL')
-                delete_message_ids.append({'Id': one_record.message_id, 'ReceiptHandle': one_record.receipt_handle})
+                key_name = 'temperature+{}+{}'.format(one_entry['sensor_name'].upper(), dt_obj.date().strftime('%Y%m'))
+                ddb.put_item(TableName='dataTable', Item={
+                    'key_name': {"S": key_name},
+                    'timestamp': {"S": dt_str},
+                    'reading_value': {"N": str(one_entry['raw_value'])}
+                }, ReturnConsumedCapacity='TOTAL')
+            delete_message_ids.append({'Id': one_record.message_id, 'ReceiptHandle': one_record.receipt_handle})
     queue.delete_messages(Entries=delete_message_ids)
-
-
-#ambda_handler(test_events, None)
