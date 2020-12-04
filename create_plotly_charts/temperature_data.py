f460af700213779d5932c99f4336c14e18952cdd
Avi <avioza@gmail.com>
2020-12-04 10:53:50 -0500

    change code to write to s3 and put into class

--- create_plotly_charts/temperature_data.py
+++ create_plotly_charts/temperature_data.py
@@ -1,8 +1,8 @@
+import argparse
 import io
 import logging
 
-import arrow
-import argparse
+import boto3
 import pandas as pd
 import plotly.express as px
 import requests
@@ -12,42 +12,63 @@ pd.options.plotting.backend = "plotly"
 logger = logging.getLogger(__name__)
 
 
-def build_temperature_chart(df):
-    fig = px.line(df, x='timestamp', y='value', line_group='location', color='location',
-                  title='Temperature')
-    buf = io.BytesIO()
-    fig.write_image(buf, format='png', scale=0.75)
-    buf.seek(0)
+class TemperatureData:
 
-    return buf
+    @staticmethod
+    def build_temperature_chart(df):
+        fig = px.line(df, x='timestamp', y='value', line_group='location', color='location',
+                      title='Temperature')
+        buf = io.BytesIO()
+        fig.write_image(buf, format='png', scale=0.75)
+        buf.seek(0)
 
+        return buf
 
-def _get_temperatures_for_range(api_key, api_url, sd, ed, location):
-    params = {
-        'limit': 9999,
-        'freq': 'D',
-        'sd_str': sd.strftime('%Y-%m-%d'),
-        'ed_str': ed.strftime('%Y-%m-%d') if not isinstance(ed, str) else ed,
-    }
+    @staticmethod
+    def write_image_to_s3(bucket, file_path, img_buffer):
+        s3 = boto3.client('s3')
+        s3.upload_fileobj(img_buffer, bucket, file_path)
 
-    result = []
-    s = requests.Session()
-    s.headers.update({'X-Api-Key': api_key})
+    @classmethod
+    def get_temperatures(cls, api_key, api_url, sd, ed, locations):
 
-    resp = s.get(fr'{api_url}/temperatures/{location}/ts', params=params)
-    r = resp.json()
-    if 'data' in r and r['data']:
-        result.extend(r['data'])
+        df_list = []
+        for loc in locations:
+            one_df = cls._get_temperatures_for_range(api_key,
+                                                     api_url,
+                                                     sd, ed, loc)
+            df_list.append(one_df)
 
-    df = pd.DataFrame.from_records(result, columns=['timestamp', 'value'])
-    df['timestamp'] = pd.to_datetime(df['timestamp'])
-    df['value'] = df['value'].astype(float)
-    df['location'] = location
-    return df.sort_values('timestamp')
+        df_total = pd.concat(df_list)
+
+        return df_total
+
+    @classmethod
+    def _get_temperatures_for_range(cls, api_key, api_url, sd, ed, location):
+        params = {
+            'limit': 9999,
+            'freq': 'D',
+            'sd_str': sd.strftime('%Y-%m-%d'),
+            'ed_str': ed.strftime('%Y-%m-%d') if not isinstance(ed, str) else ed,
+        }
+
+        result = []
+        s = requests.Session()
+        s.headers.update({'X-Api-Key': api_key})
+
+        resp = s.get(fr'{api_url}/temperatures/{location}/ts', params=params)
+        r = resp.json()
+        if 'data' in r and r['data']:
+            result.extend(r['data'])
+
+        df = pd.DataFrame.from_records(result, columns=['timestamp', 'value'])
+        df['timestamp'] = pd.to_datetime(df['timestamp'])
+        df['value'] = df['value'].astype(float)
+        df['location'] = location
+        return df.sort_values('timestamp')
 
 
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser()
     parser.add_argument('--lambda-api-key', type=str)
     parser.add_argument('--lambda-url', type=str)
@@ -60,15 +81,12 @@ if __name__ == '__main__':
     ed = pd.Timestamp.today()
     sd = ed - Day(7)
 
-    df_list = []
-    for loc in args.locations:
-        one_df = _get_temperatures_for_range(args.lambda_api_key,
-                                             args.lambda_url,
-                                             sd, ed, loc)
-        df_list.append(one_df)
+    # df = TemperatureData.get_temperatures(args.lambda_api_key, args.lambda_url, sd, ed, args.locations)
+    # df.to_pickle('sample_data.pkl')
+    df = pd.read_pickle('sample_data.pkl')
 
-    df_total = pd.concat(df_list)
+    # chart_bytes = TemperatureData.build_temperature_chart(df)
 
-    data = build_temperature_chart(df_total)
-    with open('test.png', 'wb') as f:
-        f.write(data.read())
+    # TemperatureData.write_image_to_s3(args.bucket, args.file_path, chart_bytes)
+    # with open('test.png', 'wb') as f:
+    #     f.write(chart_bytes.read())
